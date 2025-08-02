6bc50cd18093014a7f720349b1aea7fcc5baef88
itahara54@gmail.com <itahara54@gmail.com>
2025-08-03 07:24:10 +0900

    del sv

--- app/photo/xPhotosData.js
+++ app/photo/xPhotosData.js
@@ -1,33 +1,8 @@
 const xPhotosData = [
     {
-      postUrl: "https://x.com/Astemo_Rivale/status/1885939768607048077",
-      imageUrl: "https://pbs.twimg.com/media/Giw0OWIbEAADRsO?format=jpg&name=medium",
-    },
-    {
-      postUrl: "https://x.com/Astemo_Rivale/status/1886004311349698627",
-      imageUrl: "https://pbs.twimg.com/media/Gixu7PSaEAA2ZE6?format=jpg&name=large",
-    },
-    {
       postUrl: "https://x.com/entameclipnews/status/1891993426679476376",
       imageUrl: "https://pbs.twimg.com/media/GkG11QvaAAIuxbu?format=jpg&name=medium",
     },
-    {
-      postUrl: "https://x.com/Astemo_Rivale/status/1898293741208875276",
-      imageUrl: "https://pbs.twimg.com/media/GlgOfPdbwAAUy1g?format=jpg&name=medium",
-    },
-    {
-      postUrl: "https://x.com/Astemo_Rivale/status/1898311490198962220",
-      imageUrl: "https://pbs.twimg.com/media/GlgoPTpbUAErtJj?format=jpg&name=large",
-    },
-    {
-      postUrl: "https://x.com/Astemo_Rivale/status/1898637316467708401",
-      imageUrl: "https://pbs.twimg.com/media/GllQkx7bAAAGFaj?format=jpg&name=medium",
-    },
-    {
-      postUrl: "https://x.com/Astemo_Rivale/status/1898675070773362755",
-      imageUrl: "https://pbs.twimg.com/media/Glly6gDbwAALBEt?format=jpg&name=large",
-    },
-
     // さらに必要なだけ追加
   ];
   
