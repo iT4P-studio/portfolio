9b89677bab45cd5ccc8247b7e5cf8d922eff58be
iT4P-studio <itahara54@gmail.com>
2026-01-06 13:50:00 +0900

    build error ha

--- next.config.mjs
+++ next.config.mjs
@@ -3,14 +3,12 @@ const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
-    // もし外部ホストを使う場合、remotePatternsやdomainsを設定していてもOK
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'pbs.twimg.com',
       },
     ],
-    // domains: ['pbs.twimg.com'], // 旧式設定でも可
   },
 };
 
